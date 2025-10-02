218ab485d26a084e7c1e733c906f4b81f7125242
pramono <pramono6236@gmail.com>
2025-10-02 21:03:03 +0700

    feat: enhance Login component with improved styling and error handling

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="flex justify-center">
-    <div class="card">
+    <div class="card w-1/3 bg-base-200">
       <div class="card-body">
         <h1 class="card-title">Login</h1>
         <div class="divider"></div>
         <form @submit.prevent="submit">
           <fieldset class="fieldset">
             <legend class="fieldset-legend">Email</legend>
-            <input v-model="email" type="email" class="input" placeholder="Type here" />
+            <input v-model="email" type="email" class="input w-full" placeholder="Type here" />
           </fieldset>
           <fieldset class="fieldset">
             <legend class="fieldset-legend">Password</legend>
-            <input v-model="password" type="password" class="input" placeholder="Type here" />
+            <input v-model="password" type="password" class="input w-full" placeholder="Type here" />
           </fieldset>
           <div class="divider"></div>
-          <button type="submit" class="btn btn-primary">Login</button>
+          <button type="submit" class="btn btn-primary" :disabled="loading">{{ loading ? "Login..." : "Login"
+          }}</button>
         </form>
       </div>
     </div>
@@ -25,18 +26,25 @@
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
+import { useToast } from 'vue-toastification'
 
 const email = ref('')
 const password = ref('')
 const router = useRouter()
 const auth = useAuthStore()
-
+const loading = ref(false)
+const toast = useToast()
 const submit = async () => {
+  loading.value = true
   try {
     await auth.login({ email: email.value, password: password.value })
+    loading.value = false
     router.push({ name: 'dashboard' })
-  } catch (e) {
-    alert('Login gagal')
+  } catch (err) {
+    loading.value = false
+    console.log(err.response.data.message)
+    toast.error("Login gagal " + err.response.data.message)
+
   }
 }
 </script>
